40620c6fdf72cbc4083addeb422d272468c58ee2
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-03-13 19:43:23 +0700

    Add files via upload

---
+++ storage/framework/views/720012a9dd76e9894ccedaae05e2d4b68a7a3e00.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Starter</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="<?php echo e(asset('plugins/fontawesome-free/css/all.min.css')); ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo e(asset('dist/css/adminlte.min.css')); ?>">
+    <!-- Extra CSS -->
+    <?php echo $__env->yieldContent('ExtraCSS'); ?>
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <?php echo $__env->make('layouts.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <?php echo $__env->yieldContent('web-content'); ?>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+        <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+        </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?php echo e(asset('plugins/jquery/jquery.min.js')); ?>"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo e(asset('plugins/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo e(asset('dist/js/adminlte.min.js')); ?>"></script>
+<!-- Extra JavaScript -->
+<?php echo $__env->yieldContent('ExtraJS'); ?>
+</body>
+</html>
+<?php /**PATH D:\xampp\htdocs\project-uts\resources\views/layouts/master.blade.php ENDPATH**/ ?>
