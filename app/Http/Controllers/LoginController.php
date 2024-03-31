6fbb6e61654e1f4fbf8c9c9703acf7beb8a08b71
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-03-31 21:26:36 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,11 +8,9 @@ use Auth;
 
 class LoginController extends Controller
 {
-//    public function login(Request $request){
-////        dd($request->all());
-//        if(Auth::attempt($request->only('email','password'))){
-//            return redirect('/starter');
-//        }
-//        return redirect('login');
-//    }
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
 }
