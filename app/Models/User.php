9642516ed1323798c66d74a9987b858d3a628776
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2024-03-31 21:29:09 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $primaryKey = 'id';
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
 }
