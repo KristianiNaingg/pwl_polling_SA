8fe18efa5872641a184abdd6918116b23acd50a5
Theresia <90782555+2172017@users.noreply.github.com>
2024-04-01 19:24:39 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MatkulController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,23 +16,27 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::get('/', function() {
-    return redirect(route('login'));
+Route::get('/', function () {
+    return view('welcome');
 });
 
 Route::get('/starter', function(){
     return view('starter' );
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [\App\Http\Controllers\MataKuliahController::class, 'index'])->name('dashboard');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate'])->name('login');
+//Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
 
 require __DIR__.'/auth.php';
+Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi-list');
+Route::get('/prodi/create', [ProdiController::class, 'create'])->name('prodi-create');
+Route::post('/prodi-create', [ProdiController::class, 'store'])->name('prodi-store');
 
-Route::get('/mk', [\App\Http\Controllers\MataKuliahController::class,'index'])->name('mk-list');
-Route::get('/mk-polling', [\App\Http\Controllers\MataKuliahController::class,'polling'])->name('mk-polling');
-Route::post('/mk-store', [\App\Http\Controllers\MataKuliahController::class,'store'])->name('mk-store');
 
+Route::get('/prodi/matkul', [MatkulController::class, 'index'])->name('matkul-list');
+
+Route::get('/prodi/matkul-create', [MatkulController::class, 'create'])->name('matkul-create');
+Route::post('/prodi/matkul-store', [MatkulController::class, 'store'])->name('matkul-store');
