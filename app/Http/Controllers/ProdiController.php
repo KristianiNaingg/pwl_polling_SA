d7d2b9ab16f4e02ec104549f187c693fe5bfa93b
Theresia <90782555+2172017@users.noreply.github.com>
2024-04-17 19:07:28 +0700

    ProdiController.php

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -22,8 +22,11 @@ class ProdiController extends Controller
     public function create()
         #menampilkan formnya saja
     {
-        return view('prodi.create');
-        #kk.created harus sesuai dengan nama file di directory kk
+        $lastId = Prodi::max('id');
+        // Menambah 1 untuk mendapatkan id berikutnya
+        $nextId = $lastId + 1;
+        return view('prodi.create',(compact('nextId')));
+
     }
 
 
@@ -54,5 +57,68 @@ class ProdiController extends Controller
         $prodi->save();
 
         // Redirect ke halaman lain atau kembali ke halaman sebelumnya
-        return redirect(route('prodi-list'));    }
+        return redirect(route('prodi-list'));
+    }
+
+
+    public function show(Prodi $prodi)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prodi $prodi
+     * @return \Illuminate\Http\Response
+     */
+
+    public function edit(Prodi $prodi)
+    {
+        return view('prodi.edit', [
+            'prodi' => $prodi,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Prodi $prodi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Prodi $prodi)
+    {
+        $validatedData = $request->validate([
+            'id' => 'required|string|max:16|unique:polling',
+            'polling' => 'required|max:100',
+            'tanggalbuka' => 'required|date',
+            'tanggaltutup' => 'required|date',
+         // Mengganti 'polling' menjadi 'nama_polling' sesuai dengan nama kolom dalam tabel
+        ], [
+            'id.required' => ' Id Wajib diisi',
+            'polling.required' => 'Polling wajib diisi',
+            'tanggalbuka.required' => 'Tanggal  Buka wajib diisi',
+            'tanggaltutup.required' => 'Tanggal Tutup  wajib diisi',
+        ]);
+
+        $prodi->id = $validatedData['id'];
+        $prodi->nama_polling = $validatedData['polling']; // Pastikan 'nama_polling' diisi dengan nilai yang valid
+        $prodi->tgl_buka = $validatedData['tanggalbuka'];
+        $prodi->tgl_tutup = $validatedData['tanggaltutup'];
+        $prodi->save();
+
+        return redirect(route('prodi-list'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Prodi $prodi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prodi $prodi)
+    {
+        $prodi->delete();
+        return redirect(route('prodi-list'));
+    }
 }
