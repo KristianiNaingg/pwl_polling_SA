567f751b97d106647232f9dc5458547e8ebf2fd0
Theresia <90782555+2172017@users.noreply.github.com>
2024-04-23 11:43:15 +0700

    index.blade.php
    ubah isi index.blade. php

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -27,6 +27,13 @@
                 <div class="card-body">
                     <a href="{{route('prodi-mkcreate')}}" class="btn btn-success">Tambah MataKuliah </a>
                 </div>
+
+            </div>
+            <div class="container-fluid">
+                <div class="card-body">
+                    <a href="" class="btn btn-success">Hasil Polling <a>
+                </div>
+
             </div>
 
         </div>
@@ -36,6 +43,60 @@
                     <div class="card-body">
                         <table id="table-matkul" class="table table-striped">
                             <thead>
+
+                            <tr>
+
+                                <th>No</th>
+                                <th>Nama Polling</th>
+                                <th>Tanggal Buka</th>
+                                <th>Tanggal Tutup</th>
+                                <th>Aksi</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($pollings as  $polling)
+                                <tr>
+                                    <td>{{ $polling->id_polling }}</td>
+                                    <td>{{ $polling->nama_polling }}</td>
+                                    <td>{{ $polling->tgl_buka }}</td>
+                                    <td>{{ $polling->tgl_tutup}}</td>
+
+                                    <td>
+
+
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </td>
+
+{{--                                    <td>--}}
+{{--                                        <a href="{{ route('prodi-mkedit', ['matkul' => $matkul->id_matkul]) }}" class="btn btn-warning" role="button"><i class="fas fa-edit"></i></a>--}}
+{{--                                        <a href="{{ route('prodi-mkupdate', ['matkul' => $matkul->id_matkul]) }}" class="btn btn-danger del-button" role="button" data-confirm="Apakah kamu yakin menghapus ini?"><i class="fas fa-trash"></i></a>--}}
+{{--                                    </td>--}}
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </div>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="card p-4">
+                    <div class="card-body">
+                        <table id="table-matkul" class="table table-striped">
+                            <thead>
+
                             <tr>
                                 <th>No</th>
                                 <th>Kode Matkul</th>
@@ -43,22 +104,67 @@
                                 <th>SKS</th>
                                 <th>Tahun Akademik</th>
                                 <th>Semester</th>
+                                <th>Nama Polling</th>
+                                <th>Tanggal Buka</th>
+                                <th>Tanggal Tutup</th>
+                                <th>Aksi</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($matkuls as  $matkul)
                                 <tr>
                                     <td>{{ $matkul->id_matkul }}</td>
-
                                     <td>{{ $matkul->kode_matkul }}</td>
                                     <td>{{ $matkul->nama_matkul }}</td>
                                     <td>{{ $matkul->sks }}</td>
                                     <td>{{ $matkul->kurikulum->tahun_akademik }}</td>
                                     <td>{{ $matkul->kurikulum->semester }}</td>
+
+
+
+                                    <td>
+                                        <select name="polling_id" id="polling_id" class="form-control @error('polling_id') is-invalid @enderror">
+                                            <option value="">- Pilih kurikulum -</option>
+                                            @foreach ($pollings as $polling)
+                                                <option value="{{ $polling->id_polling }}">{{$polling->nama_polling }}</option>
+                                            @endforeach
+                                        </select>
+
+                                    </td>
+
+
+
+                                    <td>
+                                        <div class="form-group row">
+                                            <div class="col-sm-10">
+{{--                                                <input type="date" class="form-control" id="tgl_buka" name="tgl_buka" readonly>--}}
+                                                <input type="text" class="form-control" name="tgl_buka" id="tgl_buka" placeholder="Tanggal Buka" >
+                                            </div>
+                                        </div>
+
+                                    </td>
+
+                                    <td>
+                                        <div class="form-group row">
+                                            <div class="col-sm-10">
+                                                <input type="date" class="form-control" id="tgl_tutup" name="tgl_tutup" readonly>
+                                            </div>
+                                    </td>
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </td>
+
+                                    <td>
+                                        <a href="{{ route('prodi-mkedit', ['matkul' => $matkul->id_matkul]) }}" class="btn btn-warning" role="button"><i class="fas fa-edit"></i></a>
+                                        <a href="{{ route('prodi-mkupdate', ['matkul' => $matkul->id_matkul]) }}" class="btn btn-danger del-button" role="button" data-confirm="Apakah kamu yakin menghapus ini?"><i class="fas fa-trash"></i></a>
+                                    </td>
                                 </tr>
                             @endforeach
                             </tbody>
                         </table>
+
                     </div>
                 </div>
             </div><!-- /.container-fluid -->
@@ -70,14 +176,51 @@
 @section('ExtraCSS')
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{asset('project-03/public/plugins/sweetalert2/sweetalert2.css')}}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+
+
 @endsection
 
 @section('ExtraJS')
     <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+{{--    <script>--}}
+{{--        $(document).ready(function() {--}}
+{{--            $('#table-matkul').DataTable();--}}
+{{--        });--}}
+
+{{--        $(document).ready(function(){--}}
+{{--            $('.datepicker').datepicker({--}}
+{{--                format: 'yyyy-mm-dd',--}}
+{{--                autoclose: true--}}
+{{--            });--}}
+{{--            $('#table-prodi').DataTable();--}}
+{{--        });--}}
+{{--    </script>--}}
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#table-matkul').DataTable();
+            $('#id_polling').change(function() {
+                var idPolling = $(this).val();
+
+                // Kirim permintaan AJAX ke backend
+                $.ajax({
+                    url: '/get-polling-details', // Ganti dengan URL yang sesuai
+                    method: 'GET',
+                    data: { id: id_Polling },
+                    success: function(response) {
+                        // Perbarui nilai tgl_buka dan tgl_tutup
+                        $('#tgl_buka').val(response.tgl_buka);
+                        $('#tgl_tutup').val(response.tgl_tutup);
+                    },
+                    error: function(xhr) {
+                        console.log(xhr.responseText);
+                    }
+                });
+            });
         });
     </script>
+
 @endsection
